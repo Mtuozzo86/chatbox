87f267b86b7e4b4d49bbee6cddf1bed3bedd4d7b
Michael Tuozzo <mtuozzo86@gmail.com>
2022-08-28 18:12:52 -0400

    looking decent

--- client/src/Components/ChatInput.jsx
+++ client/src/Components/ChatInput.jsx
@@ -5,7 +5,7 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
+import Card from "react-bootstrap/Card";
 
 function ChatInput({ roomTitle = "The Main Group" }) {
   useEffect(() => {
@@ -15,7 +15,7 @@ function ChatInput({ roomTitle = "The Main Group" }) {
   }, []);
 
   const [messages, setMessages] = useState([]);
-  const [inputMessage, setInputMessage] = useState("")
+  const [inputMessage, setInputMessage] = useState("");
 
   const listOfMessages = messages.map((message) => {
     return (
@@ -28,29 +28,33 @@ function ChatInput({ roomTitle = "The Main Group" }) {
   });
 
   function handleSubmit(e) {
-    e.preventDefault()
-    fetch('/chat_messages', {
+    e.preventDefault();
+    fetch("/chat_messages", {
       method: "POST",
       headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
       },
-      body: JSON.stringify({inputMessage})
-    })
+      body: JSON.stringify({ inputMessage }),
+    });
   }
-
 
   return (
     <>
       <Container fluid>
         <div className="mb-4 p-1 border-bottom">{roomTitle}</div>
-        <div className="overflow-auto pb-5">{listOfMessages}</div>
+        <Card>
+          <Card.Body className="overflow-auto" style={{height: 450}}>{listOfMessages}</Card.Body>
+        </Card>
       </Container>
       <Container className="fixed-bottom p-2">
         <Form onSubmit={handleSubmit}>
           <Row>
             <Col className="col-9">
               <Form.Group>
-                <Form.Control type="text" onChange={(e) => setInputMessage(e.target.value)} />
+                <Form.Control
+                  type="text"
+                  onChange={(e) => setInputMessage(e.target.value)}
+                />
               </Form.Group>
             </Col>
             <Col className="col-2">
