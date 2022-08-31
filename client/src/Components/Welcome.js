7dfa9a8d513c33181711aa65c37bb9bbce8de559
Michael Tuozzo <mtuozzo86@gmail.com>
2022-08-30 22:00:18 -0400

    signup/create page styling

--- client/src/Components/Welcome.js
+++ client/src/Components/Welcome.js
@@ -1,16 +1,35 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { NavLink } from "react-router-dom";
+import { RiKakaoTalkLine } from "react-icons/ri";
+import Button from "react-bootstrap/Button";
+import { LinkContainer } from "react-router-bootstrap";
+import Container from "react-bootstrap/esm/Container";
 
 function Welcome() {
+  const linkStyle = {
+    textDecoration: "none",
+    color: "black",
+  };
+
+  const iconStyle = {
+    color: "01BAEF",
+  };
   return (
-    <div>
-      <div>
-        <Link to="login">Login</Link>
+    <Container>
+      <div className="text-center mt-5">
+        <RiKakaoTalkLine style={iconStyle} size={50} />
       </div>
-      <div>
-        <Link to="create">Create</Link>
+      <div className="d-grid gap-2 text-center mt-5">
+        <NavLink style={linkStyle} to="login">
+          <Button className="list-group-item-action">Login</Button>
+        </NavLink>
+        <NavLink style={linkStyle} to="create">
+          <Button className="list-group-item-action btn-outline-primary" variant="dark-outline">
+            Create
+          </Button>
+        </NavLink>
       </div>
-    </div>
+    </Container>
   );
 }
 
