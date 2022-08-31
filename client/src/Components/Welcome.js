7e8155df371ede9547d0e53d08bfb84b85126059
Michael Tuozzo <mtuozzo86@gmail.com>
2022-08-31 10:38:59 -0400

    decent create/signup page

--- client/src/Components/Welcome.js
+++ client/src/Components/Welcome.js
@@ -15,16 +15,19 @@ function Welcome() {
     color: "01BAEF",
   };
   return (
-    <Container>
-      <div className="text-center mt-5">
-        <RiKakaoTalkLine style={iconStyle} size={50} />
+    <Container className="position-relative" style={{ height: 500 }}>
+      <div className="text-center my-5">
+        <RiKakaoTalkLine style={iconStyle} size={100} />
       </div>
-      <div className="d-grid gap-2 text-center mt-5">
+      <div className="d-grid gap-2 text-center mx-auto my-5 w-75 position-absolute top-50 start-50 translate-middle">
         <NavLink style={linkStyle} to="login">
           <Button className="list-group-item-action">Login</Button>
         </NavLink>
         <NavLink style={linkStyle} to="create">
-          <Button className="list-group-item-action btn-outline-primary" variant="dark-outline">
+          <Button
+            className="list-group-item-action btn-outline-primary"
+            variant="dark-outline"
+          >
             Create
           </Button>
         </NavLink>
