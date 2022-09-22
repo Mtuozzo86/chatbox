256cd6f6b152d6a2d2cf7c63e334fd1f2ee944f4
Michael Tuozzo <mtuozzo86@gmail.com>
2022-09-22 00:15:24 -0400

    still

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount ActionCable.server => "/cable"
   resources :users, only: [:index, :create, :show]
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :conversations, only: [:index, :show] do
     resources :chat_messages, only: [:index, :create]
   end
-  
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
   get '/me', to: "sessions#show"
