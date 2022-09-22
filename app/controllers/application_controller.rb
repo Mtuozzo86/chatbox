e0649502b862c0c74ce063058b061c0ab3ee2237
Michael Tuozzo <mtuozzo86@gmail.com>
2022-09-21 23:44:55 -0400

    trying deploy by readding fallback controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::API
 include ActionController::Cookies
-def fallback_index_html
-  render :file => 'public/index.html'
-end
+
 
 
     def current_user
