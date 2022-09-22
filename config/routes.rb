638aac73d8683280f85e542c2d9ff653302f01d0
Michael Tuozzo <mtuozzo86@gmail.com>
2022-09-21 23:18:33 -0400

    trying to deploy with correct route, build was successful

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount ActionCable.server => "/cable"
   resources :users, only: [:index, :create, :show]
