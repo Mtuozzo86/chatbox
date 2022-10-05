d785b4888ab6e153ecd827846b2a29e8e0a914f0
Mtuozzo86 <Mtuozzo86@gmail.com>
2022-10-05 14:29:26 -0400

    config changes to mount action cable in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,9 +34,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.mount_path = "/cable"
+  config.action_cable.url = 'wss://mikes-first-chat.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://mikes-first-chat.herokuapp.com', /http:\/\/mikes-first-chat.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
