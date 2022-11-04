17aacf3070ef27f130522de435e7e0733cb332d7
Mtuozzo86 <mtuozzo86@gmail.com>
2022-11-04 11:30:29 -0400

    might have associations correct for a user to create a room without needing the chat message model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,9 +1,4 @@
 class Conversation < ApplicationRecord
+   has_and_belongs_to_many :users
    has_many :chat_messages
-   has_many :users, through: :chat_messages
-<<<<<<< HEAD
-   has_one  :admin, class_name: 'User',foreign_key: 'user_id'
 end
-=======
-end
->>>>>>> fe6b3550265ab5b2e238c46dde3b2ece958d0575
